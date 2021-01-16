9bf6fb7899880ae6eb86f81ff1ed671aa1561591
Alec Loudenback <alecloudenback@gmail.com>
2021-01-15 22:00:55 -0600

    fix depreciation in DataFrames

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using CSV
 using Test
 using DataFrames
 
-raa = CSV.File("data/raa.csv") |> DataFrame!
+raa = CSV.File("data/raa.csv") |> DataFrame
 @testset "ChainLadder.jl" begin
     t = ClaimTriangle(raa,:origin,:development,:values,IncrementalTriangle())
     @test all(latest_diagonal(t) .== [18834.0,16704.0,23466.0,27067.0,26180.0,15852.0,12314.0,13112.0,5395.0,2063.0])
