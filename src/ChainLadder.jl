229eab2f2595ebd454db6eee8dff2971d7a23177
Alec Loudenback <alecloudenback@gmail.com>
2021-01-15 22:20:35 -0600

    add link_ratio

--- src/ChainLadder.jl
+++ src/ChainLadder.jl
@@ -4,7 +4,8 @@ using DataFrames
 
 export ClaimTriangle,
     latest_diagonal,
-    IncrementalTriangle, CumulativeTriangle
+    IncrementalTriangle, CumulativeTriangle,
+    link_ratio
 
 abstract type TriangleType end
 
@@ -36,5 +37,16 @@ function latest_diagonal(t::ClaimTriangle)
 	return [v[end] for v in t.values]
 end
 
+function link_ratio(v::Vector)
+	lr = zeros(length(v)-1)
+	for i in 2:length(v)
+		lr[i-1] = v[i] / v[i-1]
+	end
+	return lr
+end
+	
+function link_ratio(t::ClaimTriangle)
+    return [link_ratio(v) for v in t.values[1:end-1]]
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,4 +7,14 @@ raa = CSV.File("data/raa.csv") |> DataFrame
 @testset "ChainLadder.jl" begin
     t = ClaimTriangle(raa,:origin,:development,:values,IncrementalTriangle())
     @test all(latest_diagonal(t) .== [18834.0,16704.0,23466.0,27067.0,26180.0,15852.0,12314.0,13112.0,5395.0,2063.0])
+
+    lr1 = link_ratio(t.values[1])
+    lrend =  link_ratio(t.values[end-1])
+    @test all(lr1 .== [1.6498403830806065, 1.319022856451808, 1.0823324470523517, 1.1468869123252858, 1.1951399660240787, 1.1129720042024598, 1.0332611472041757, 1.002901977644024, 1.0092165898617511])
+    @test all(lrend .== [1.7219917012448134])
+
+    lr = link_ratio(t)
+
+    @test all(lr1 .== lr[1])
+    @test all(lrend .== lr[end])
 end
